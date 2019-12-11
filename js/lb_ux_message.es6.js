564023ce0de027b93cec7b2f564a314b37a98c8d
zrpnr <peter.weber@acquia.com>
2019-12-11 10:08:48 -0700

    Refactor use of drupalSettings for storage.

--- js/lb_ux_message.es6.js
+++ js/lb_ux_message.es6.js
@@ -4,92 +4,106 @@
  */
 
 (($, Drupal, drupalSettings) => {
+  const selectors = {
+    wrapper: {
+      initial: "data-drupal-messages",
+      active: "js-messages__wrapper"
+    },
+    message: {
+      id: "data-drupal-message-id",
+      active: "messages__closeable",
+      button: "drupal-message-close"
+    }
+  };
+
+  const lbMessage = {
+    list: [],
+    display: []
+  };
+
+  const initWrapper = () => {
+    const { active, initial } = selectors.wrapper;
+    const { button } = selectors.message;
+
+    let wrapper = document.querySelector(`.${active}`);
+    if (!wrapper) {
+      wrapper = document.querySelector(`[${initial}]`);
+      wrapper.classList.add(active);
+    }
+
+    if (!lbMessage.hasOwnProperty("message")) {
+      lbMessage.message = new Drupal.Message(wrapper);
+      wrapper.addEventListener("click", event => {
+        if (event.target.classList.contains(button)) {
+          const id = event.target.dataset.drupalMessageId;
+          lbMessage.message.remove(id);
+        }
+      });
+    }
+  };
+
+  const buttonClose = id => {
+    const { id: idSelector, button: buttonSelector } = selectors.message;
+    const button = document.createElement("button");
+    button.innerHTML = `<span class="visually-hidden">${Drupal.t(
+      "Close"
+    )}</span>`;
+    button.setAttribute(idSelector, id);
+    button.classList.add(buttonSelector);
+
+    return button;
+  };
+
+  const displayMessages = messageList => {
+    initWrapper();
+    // Filter list for already added messages and restructure object.
+    const messageQueue = messageList.reduce((queue, list) => {
+      Object.keys(list).forEach(type => {
+        list[type]
+          .filter(
+            message =>
+              !lbMessage.display.find(display => message === display.text)
+          )
+          .forEach(message => {
+            queue.push({ message, type });
+          });
+      });
+      return queue;
+    }, []);
+
+    // Add messages in the queue.
+    messageQueue.forEach((item, index) => {
+      const { message: messageText, type } = item;
+      const { id: idSelector, active } = selectors.message;
+      const id = lbMessage.message.add(messageText, {
+        priority: type,
+        type
+      });
+
+      const message = document.querySelector(`[${idSelector}=${id}]`);
+      message.classList.add(active);
+      message.style.setProperty("--animation-index", index);
+      message.appendChild(buttonClose(id));
+
+      lbMessage.display.push({
+        text: messageText,
+        type,
+        id
+      });
+    });
+  };
+
   /**
    *
    * @type {Drupal~behavior}
    */
   Drupal.behaviors.LbUXMessage = {
     attach: function() {
-      const initWrapper = () => {
-        let wrapper = document.querySelector(".js-messages__wrapper");
-        if (!wrapper) {
-          wrapper = document.querySelector("[data-drupal-messages]");
-          wrapper.classList.add("js-messages__wrapper");
-        }
-
-        if (drupalSettings.lbUX) {
-          if (!drupalSettings.lbUX.hasOwnProperty("messages")) {
-            drupalSettings.lbUX.messages = new Drupal.Message(wrapper);
-            wrapper.addEventListener("click", event => {
-              if (event.target.classList.contains("drupal-message-close")) {
-                const id = event.target.dataset.drupalMessageId;
-                drupalSettings.lbUX.messages.remove(id);
-              }
-            });
-          }
-        }
-
-        return wrapper;
-      };
-
-      const displayMessages = messageList => {
-        const messagesWrapper = initWrapper();
-        if (!messagesWrapper || !(drupalSettings.lbUX || {}).messages) {
-          return;
-        }
-        const messageQueue = messageList.reduce((queue, list) => {
-          Object.keys(list).forEach(type => {
-            list[type]
-              .filter(
-                message =>
-                  (!drupalSettings.lbUX.messageDisplay.find(
-                    display => message === display.text
-                  ))
-              )
-              .forEach(message => {
-                queue.push({ message, type });
-              });
-          });
-          return queue;
-        }, []);
-
-        messageQueue.forEach((item, index) => {
-          const { message, type } = item;
-          const id = drupalSettings.lbUX.messages.add(message, {
-            priority: type,
-            type
-          });
-          const messageClose = document.createElement("button");
-          messageClose.innerHTML = `<span class="visually-hidden">${Drupal.t(
-            "Close"
-          )}</span>`;
-          messageClose.setAttribute("data-drupal-message-id", id);
-          messageClose.classList.add("drupal-message-close");
-
-          const messageWrapper = document.querySelector(
-            `[data-drupal-message-id=${id}]`
-          );
-          messageWrapper.classList.add("messages__closeable");
-          messageWrapper.style.setProperty("--animation-index", index);
-          messageWrapper.appendChild(messageClose);
-
-          drupalSettings.lbUX.messageDisplay.push({
-            text: message,
-            type,
-            id
-          });
-        });
-      };
-
       if (drupalSettings.hasOwnProperty("lbUX")) {
-        // Add a variable for listing displayed messages.
-        if (!drupalSettings.lbUX.hasOwnProperty("messageDisplay")) {
-          drupalSettings.lbUX.messageDisplay = [];
-        }
+        lbMessage.list = drupalSettings.lbUX.messageList;
       }
 
-      const { messageList = [] } = drupalSettings.lbUX || {};
-      displayMessages(messageList);
+      displayMessages(lbMessage.list);
     }
   };
 })(jQuery, Drupal, drupalSettings);
